c9020fd35734e68aae06cc37bc46df833e05a2a9
Alex <git@charlieheart.me>
2019-08-12 13:02:18 +0200

    add started field

--- Answers.cs
+++ Answers.cs
@@ -4,6 +4,7 @@ namespace boardgame_bot
 {
     internal class Answers
     {
+        public bool Started;
         public Nullable<int> NumberOfPlayers;
         public bool WaitingForNumberOfPlayers;
     }
