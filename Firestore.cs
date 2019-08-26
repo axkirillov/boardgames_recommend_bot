c87b951f935d0d47ee88263f0bc55ded6856ad6c
Alex <git@charlieheart.me>
2019-08-26 20:32:59 +0300

    make collection private

--- Firestore.cs
+++ Firestore.cs
@@ -4,11 +4,10 @@ namespace boardgame_bot
     public static class Firestore
     {
         static FirestoreDb db;
-        static CollectionReference collection;
         public static CollectionReference Connect()
         {
             db = FirestoreDb.Create("boardgame-recommend-bot");
-            collection = db.Collection("games");
+            var collection = db.Collection("games");
             return collection;
         }
     }
