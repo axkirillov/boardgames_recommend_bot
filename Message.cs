0bd697466852fe78cf3fe7cfa4b8d09a14851403
Alex <git@charlieheart.me>
2019-09-06 21:59:18 +0300

    delete recommend monopoly

--- Message.cs
+++ Message.cs
@@ -21,14 +21,6 @@ namespace boardgame_bot
               text: "Press /start to begin");
         }
 
-        internal static async void RecommendMonopoly(MessageEventArgs e)
-        {
-            Thread.Sleep(1000);
-            await Bot.botClient.SendTextMessageAsync(
-              chatId: e.Message.Chat,
-              text: "You should play Monopoly!");
-        }
-
         internal static async void Recommend(MessageEventArgs e, Game game)
         {
             Thread.Sleep(1000);
