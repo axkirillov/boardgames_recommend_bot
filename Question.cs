2ae5e30d7920de9fd9522890dd7a3e05ddb1d005
Alex <git@charlieheart.me>
2019-09-08 23:12:35 +0300

    make2 row of buttons

--- Question.cs
+++ Question.cs
@@ -20,17 +20,21 @@ namespace boardgame_bot
         }
         internal static async void PlayTime(State state)
         {
-            InlineKeyboardButton[] row = {
+            InlineKeyboardButton[] row1 = {
                 InlineKeyboardButton.WithCallbackData("< 1 hour", "<1"),
                 InlineKeyboardButton.WithCallbackData("1-2 hours", "1-2"),
-                InlineKeyboardButton.WithCallbackData("> 2-5 hours", "2-5"),
-                InlineKeyboardButton.WithCallbackData("> 5 hours", ">2"),
+
             };
+            InlineKeyboardButton[] row2 = {
+                 InlineKeyboardButton.WithCallbackData("> 2-5 hours", "2-5"),
+                InlineKeyboardButton.WithCallbackData("> 5 hours", ">5"),
+            };
+            InlineKeyboardButton[][] rows = { row1, row2 };
             Thread.Sleep(1000);
             await Bot.botClient.SendTextMessageAsync(
               chatId: state.ChatId,
               text: "How long do you want your playing session to be?",
-              replyMarkup: new InlineKeyboardMarkup(row)
+              replyMarkup: new InlineKeyboardMarkup(rows)
             );
         }
         internal static async void Age(State state)
