73274dcae7d7460dd7375c45337ddc3f0b032791
Alex <git@charlieheart.me>
2019-09-10 17:29:40 +0300

    cleanup using

--- Program.cs
+++ Program.cs
@@ -1,15 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Google.Cloud.Storage.V1;
-using System.Diagnostics;
 using Newtonsoft.Json;
 
 namespace boardgame_bot
