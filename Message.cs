18addba4724c7d3b9c410c0373cc41343453db0b
Aleksandr Kirillov <github@charlieheart.me>
2019-09-10 21:20:42 +0300

    add a try again button

--- Message.cs
+++ Message.cs
@@ -50,7 +50,9 @@ namespace boardgame_bot
             Thread.Sleep(1000);
             await Bot.botClient.SendTextMessageAsync(
               chatId: id,
-              text: $"Sorry, we could not find a game that suits you");
+              text: $"Sorry, we could not find a game that suits you",
+              replyMarkup: new InlineKeyboardMarkup(InlineKeyboardButton.WithCallbackData("Try again", "startover"))
+            );
         }
     }
 }
